439774880fc2420d6dfa3787275357c5b4d13f09
David Gustafsson <david.gustafsson@ub.gu.se>
2018-04-24 15:49:47 +0200

    Add attachment option to logger.pl

--- logger.pl
+++ logger.pl
@@ -3,16 +3,25 @@
 use Modern::Perl;
 use Log::Log4perl;
 use Log::Log4perl::Level;
+use Log::Log4perl::MDC;
 use Getopt::Long;
 use Config::Tiny;
 use lib 'lib';
 
 # TODO: Could also support mail attachment?
-my ($level, $message, $logger_ns);
+my (
+    $level,
+    $message,
+    $logger_ns,
+    $attachment,
+    $attachment_mime_type
+);
 GetOptions(
     'level=s' => \$level,
     'message=s' => \$message,
     'logger=s' => \$logger_ns,
+    'attachment=s' => \$attachment,
+    'attachment-mime-type=s' => \$attachment_mime_type,
 );
 
 die('--level required') unless $level;
@@ -34,4 +43,12 @@ my $config = Config::Tiny->read('gobi.pl.conf', 'utf8');
 Log::Log4perl::init($config->{_}->{log4perl_config} || 'log4perl.conf');
 
 my $logger = Log::Log4perl->get_logger($logger_ns);
+
+if ($attachment) {
+    Log::Log4perl::MDC->put('attachment', $attachment);
+    if ($attachment_mime_type) {
+        Log::Log4perl::MDC->put('attachment-mime-type', $attachment_mime_type);
+    }
+}
+
 $logger->log($levels{$level}, $message);
